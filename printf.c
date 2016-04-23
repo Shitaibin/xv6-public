b3a0396b2d6e2185c194ad8974fcace59c625eb4
shitaibin <hz_stb@163.com>
2016-04-23 07:59:26 -0700

    lab1 exercise 8, v1, not tested

--- printf.c
+++ printf.c
@@ -9,6 +9,17 @@ putc(int fd, char c)
 }
 
 static void
+/**
+ * How: Transform int into character array, but in
+ * reverse order, add sign if need, to the tail,
+ * and then write it into `fd`.
+ *
+ * Params:
+ * fd: file description
+ * xx: real value
+ * base: d, o, x, p
+ * sgn: signed or unsigned
+ */
 printint(int fd, int xx, int base, int sgn)
 {
   static char digits[] = "0123456789ABCDEF";
@@ -36,26 +47,33 @@ printint(int fd, int xx, int base, int sgn)
 }
 
 // Print to the given fd. Only understands %d, %x, %p, %s.
+// Exercise 8: add %o.
 void
 printf(int fd, char *fmt, ...)
 {
   char *s;
+  /* c: current character in `fmt` */
+  /* state: meet `%` or not */
   int c, i, state;
+  /* ap: the address of the real values. why int ????? */
   uint *ap;
 
   state = 0;
-  ap = (uint*)(void*)&fmt + 1;
+  ap = (uint*)(void*)&fmt + 1; /* why? */
   for(i = 0; fmt[i]; i++){
-    c = fmt[i] & 0xff;
+    c = fmt[i] & 0xff;  /* get current */
     if(state == 0){
       if(c == '%'){
         state = '%';
       } else {
-        putc(fd, c);
+        putc(fd, c);   /* case: "abc..." */
       }
     } else if(state == '%'){
       if(c == 'd'){
         printint(fd, *ap, 10, 1);
+        ap++; /* move ap to next place */
+      } else if(c == 'o'){
+        printint(fd, *ap, 8, 1);  /* set `*ap` is a signed octal number */
         ap++;
       } else if(c == 'x' || c == 'p'){
         printint(fd, *ap, 16, 0);
@@ -73,13 +91,13 @@ printf(int fd, char *fmt, ...)
         putc(fd, *ap);
         ap++;
       } else if(c == '%'){
-        putc(fd, c);
+        putc(fd, c);  /* "%%" */
       } else {
         // Unknown % sequence.  Print it to draw attention.
         putc(fd, '%');
         putc(fd, c);
       }
-      state = 0;
+      state = 0;  // reset state
     }
   }
 }
